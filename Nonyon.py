91fcef7a6e3ae455cfaa59ffca539a63f3ec559b
Zusyaku <71582475+Zusyaku@users.noreply.github.com>
2021-10-19 19:01:05 +0700

    Add files via upload

---
+++ Nonyon.py
@@ -0,0 +1,258 @@
+#!/bin/python2
+import os
+import sys
+import time
+
+#warna
+h='\033[32;1m'
+p='\033[37;1m'
+
+#login
+
+x = 'MR.FAGHP BLACK-404/F'
+y = 'MEMEK BASAH'
+
+def login():
+    os.system('clear')
+    os.system('figlet -f big Login | lolcat')
+    user = raw_input('\033[37;1mus : ')
+    pasw = raw_input('\033[37;1mpw : ')
+    if user == x and pasw == y:
+         print 'Login Berahsil'
+         time.sleep(2)
+         sys.exit
+    else:
+         print 'Login Gagal'
+         os.system('termux-open-url https://wa.me/6281313537943/?text=Assalamualaikum+Ganteng+Apa+Pw+ny')
+         login()
+
+
+
+if __name__ == '__main__':
+       login()
+
+#Menu/Pilihan/Input
+os.system('clear')
+time.sleep(0.0)
+os.system('figlet -f slant Script 18+ | lolcat')
+print ('       ')
+print h+('      tersedia Beberapa link')
+print h+('=====================================')
+print h+('(1). link')
+print h+('(2). link')
+print h+('(3). link')
+print h+('(4). link')
+print h+('(5). link')
+print h+('(6). link')
+print h+('(7). link')
+print h+('(8). link')
+print h+('(9). link')
+print h+('(10). link')
+print h+('(11). link')
+print h+('(12). link')
+print h+('(13). link')
+print h+('(14). link')
+print h+('(15). link')
+print h+('(16). link')
+print h+('(17). link')
+print h+('(18). link')
+print h+('(19). link')
+print h+('(20). link')
+print h+('(21). link')
+print h+('(22). link')
+print h+('(23). link')
+print h+('(24). link')
+print h+('(25). link')
+print h+('(26). link')
+print h+('(27). link')
+print h+('(28). link')
+print h+('(29). link')
+print h+('(30). link')
+print h+('(31). link')
+print h+('(32). link')
+print h+('(33). link')
+print h+('(34). link')
+print h+('(35). link')
+print h+('(36). link')
+print h+('(37). link')
+print h+('(38). link')
+print h+('(39). link')
+print h+('(40). link')
+print h+('(41). Subscribe')
+print h+('(42). Exit')
+print h+('=====================================')
+kontol = input('\033[32;2m=====\n>>')
+
+if kontol == 1:
+   os.system('clear')
+   os.system('xdg-open http://185.63.253.200/watch/49134217.html')
+   
+if kontol == 2:
+   os.system('clear')
+   os.system('xdg-open http://185.63.253.200/watch/56367347.html')
+   
+if kontol == 3:
+   os.system('clear')
+   os.system('xdg-open http://185.63.253.200/watch/33768337.html')
+   
+if kontol == 4:
+   os.system('clear')
+   os.system('xdg-open http://185.63.253.200/watch/23078711.html')
+   
+if kontol == 5:
+   os.system('clear')
+   os.system('xdg-open http://185.63.253.200/watch/18645989.html')
+
+if kontol == 6:
+   os.system('clear')
+   os.system('xdg-open http://185.63.253.200/watch/19766437.html')
+
+if kontol == 7:
+   os.system('clear')
+   os.system('xdg-open http://185.63.253.200/watch/34032557.html')
+
+if kontol == 8:
+   os.system('clear')
+   os.system('xdg-open http://185.63.253.200/watch/29800555.html')
+  
+if kontol == 9:
+   os.system('clear')
+   os.system('xdg-open http://185.63.253.200/watch/19079871.html')
+
+if kontol == 10:
+   os.system('clear')
+   os.system('xdg-open http://185.63.253.200/watch/46350341.html')
+  
+if kontol == 11:
+   os.system('clear')
+   os.system('xdg-open http://185.63.253.200/watch/29664285.html')
+  
+if kontol == 12:
+   os.system('clear')
+   os.system('xdg-open http://185.63.253.200/watch/13420371.html')
+  
+if kontol == 13:
+   os.system('clear')
+   os.system('xdg-open http://185.63.253.200/watch/20465195.html')
+  
+if kontol == 14:
+   os.system('clear')
+   os.system('xdg-open http://185.63.253.200/watch/26633789.html')
+  
+if kontol == 15:
+   os.system('clear')
+   os.system('xdg-open http://185.63.253.200/watch/26633767.html')
+  
+if kontol == 16:
+   os.system('clear')
+   os.system('xdg-open http://185.63.253.200/watch/24276127.html')
+  
+if kontol == 17:
+   os.system('clear')
+   os.system('xdg-open http://185.63.253.200/watch/32536649.html')
+  
+if kontol == 18:
+   os.system('clear')
+   os.system('xdg-open http://185.63.253.200/watch/15547855.html')
+  
+if kontol == 19:
+   os.system('clear')
+   os.system('xdg-open http://185.63.253.200/watch/28215383.html')
+
+if kontol == 20:
+   os.system('clear')
+   os.system('xdg-open http://185.63.253.200/watch/13180269.html')
+  
+if kontol == 21:
+   os.system('clear')
+   os.system('xdg-open http://185.63.253.200/watch/10812804.html')
+  
+if kontol == 22:
+   os.system('clear')
+   os.system('xdg-open http://185.63.253.200/watch/13180269.html')
+  
+if kontol == 23:
+   os.system('clear')
+   os.system('xdg-open http://185.63.253.200/watch/21592913.html')
+  
+if kontol == 24:
+   os.system('clear')
+   os.system('xdg-open http://185.63.253.200/watch/14491643.html')
+  
+if kontol == 25:
+   os.system('clear')
+   os.system('xdg-open http://185.63.253.200/watch/32536649.html')
+  
+if kontol == 26:
+   os.system('clear')
+   os.system('xdg-open http://185.63.253.200/watch/15547855.html')
+  
+if kontol == 27:
+   os.system('clear')
+   os.system('xdg-open http://185.63.253.200/watch/15591465.html')
+  
+if kontol == 28:
+   os.system('clear')
+   os.system('xdg-open http://185.63.253.200/watch/14649361.html')
+  
+if kontol == 29:
+   os.system('clear')
+   os.system('xdg-open http://185.63.253.200/watch/14431823.html')
+  
+if kontol == 30:
+   os.system('clear')
+   os.system('xdg-open http://185.63.253.200/watch/14750053.html')
+  
+if kontol == 31:
+   os.system('clear')
+   os.system('xdg-open http://185.63.253.200/watch/14347505.html')
+  
+if kontol == 32:
+   os.system('clear')
+   os.system('xdg-open http://185.63.253.200/watch/21058879.html')
+  
+if kontol == 33:
+   os.system('clear')
+   os.system('xdg-open http://185.63.253.200/watch/1535447.html')
+  
+if kontol == 34:
+   os.system('clear')
+   os.system('xdg-open http://185.63.253.200/watch/1535871.html')
+  
+if kontol == 35:
+   os.system('clear')
+   os.system('xdg-open http://185.63.253.200/watch/1146967.html')
+  
+if kontol == 36:
+   os.system('clear')
+   os.system('xdg-open http://185.63.253.200/watch/9102250.html')
+  
+if kontol == 37:
+   os.system('clear')
+   os.system('xdg-open http://185.63.253.200/watch/7799405.html')
+  
+if kontol == 38:
+   os.system('clear')
+   os.system('xdg-open http://185.63.253.200/watch/9141727.html')
+  
+if kontol == 39:
+   os.system('clear')
+   os.system('xdg-open http://185.63.253.200/watch/3702135.html')
+  
+if kontol == 40:
+   os.system('clear')
+   os.system('xdg-open http://185.63.253.200/watch/32461763.html')
+  
+if kontol == 41:
+   os.system('clear')
+   os.system('xdg-open https://youtube.com/channel/UCFggfLWFCmGGyb2VH2EBfBQ')
+   os.system('subscribe')
+  
+if kontol == 42:
+   os.system('clear')
+   print h+('   _____              __  __               __ __            __        __')
+print h+('  / ___/___  ___      \ \/ /___  __  __   / //_/___  ____  / /_____  / /')
+print h+('  \__ \/ _ \/ _ \      \  / __ \/ / / /  / ,< / __ \/ __ \/ __/ __ \/ /')
+print h+(' ___/ /  __/  __/      / / /_/ / /_/ /  / /| / /_/ / / / / /_/ /_/ / /')
+print h+('/____/\___/\___/      /_/\____/\__,_/  /_/ |_\____/_/ /_/\__/\____/_/')
+print ('           ')
---
+++ Script-Dewasa.py
@@ -0,0 +1,103 @@
+#!/bin/python2
+#code by MR.FAGHP
+#mencari dosa
+#gw udh tobat cuma bikin doang gak pakek
+import os, sys, time
+
+c='\033[36;1m'
+p='\033[37;1m'
+
+#Pembersih
+
+os.system('clear')
+
+#Update Upgrade
+time.sleep(0.0)
+os.system('figlet -f slant Update')
+os.system('figlet -f slant Upgrade')
+time.sleep(3)
+os.system('clear')
+os.system('pkg update')
+time.sleep(0.0)
+os.system('clear')
+os.system('pkg upgrade')
+time.sleep(0.0)
+os.system('clear')
+
+#Login
+
+x = 'aku ingin'
+y = 'mencari dosa'
+
+def login():
+    os.system('clear')
+    user = raw_input('\033[37;1mu : ')
+    pasw = raw_input('\033[37;1mp : ')
+    if user == x and pasw == y:
+         print p+'Login Berahsil Untuk Mencari Dosa'
+         time.sleep(2)
+         sys.exit
+    else:
+         print p+'Login Gagal Pencari Dosa'
+	 time.sleep(4)
+	 os.system('termux-open-url https://wa.me/6281313537943/?text=Assalamualaikum+Hacker+Ff+Wibu+pro+Apa+Pw+Buat+Saya+Masuk+Neraka')
+         login()
+
+
+
+if __name__ == '__main__':
+       login()
+
+#Tampilan
+time.sleep(0.0)
+os.system('clear')
+time.sleep(0.0)
+os.system('toilet -f smslant -F gay WELLCOME')
+os.system('toilet -f smslant -F gay TO SCRIPT')
+os.system('toilet -f smslant -F gay 18+')
+print ('              ')
+print c+('[===============================]')
+print c+('[1]. link 1                     ')
+print c+('[2]. link 2                     ')
+print c+('[3]. link 3                     ')
+print c+('[4]. Gabung BLACK SQUAD         ')
+print c+('[5]. Subrek yt aing             ')
+print c+('[6]. Subrek yt BLACK SQUAD      ')
+print c+('[7]. exit                       ')
+print c+('[===============================]')
+print ('                              ')
+pilih = input('\033[34;1mroot\033[31;1m@localhost')
+
+if pilih == 1:
+     os.system('clear')
+     os.system('xdg-open https://www.mediafire.com/download/gzhtaj3tu4kmrd2')
+     os.system('figlet -f slant Selamat Mendosa | lolcat')
+
+elif pilih == 2:
+     os.system('clear')
+     os.system('xdg-open https://www.mediafire.com/download/0efuymlubu5w5bt')
+     os.system('figlet -f slant Selamat Mendosa | lolcat')
+
+elif pilih == 3:
+     os.system('clear')
+     os.system('xdg-open https://www.mediafire.com/download/2g07kdjfj4s5zq4')
+     os.system('figlet -f slant Selamat Mendosa | lolcat')
+
+elif pilih == 4:
+     os.system('clear')
+     os.system('xdg-open https://chat.whatsapp.com/KmtO2X5nOmpEdq4TTO1B0k')
+     os.system('figlet -f slant Gabung Lu? | lolcat')
+
+elif pilih == 5:
+     os.system('clear')
+     os.system('xdg-open https://youtube.com/channel/UCFggfLWFCmGGyb2VH2EBfBQ')
+     os.system('figlet -f slant SUBSCRIBE | lolcat')
+
+elif pilih == 6:
+     os.system('clear')
+     os.system('xdg-open https://youtube.com/channel/UCVRjgVShZhh8GfMzH59xMZg')
+     os.system('figlet -f slant SUBSCRIBE | lolcat')
+
+elif pilih == 7:
+     os.system('clear')
+     os.system('figlet -f slant Byee | lolcat')
